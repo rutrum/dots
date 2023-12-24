188e4f2846451e0ff15afdf34f85c8ad085f7dba
David Purdum <purdum41@gmail.com>
2023-12-24 15:55:24 -0500

    Add flake registries for stable and unstable nixpkgs

--- home.nix
+++ home.nix
@@ -38,11 +38,35 @@ in {
     target = ".config/xmonad/xmonad.hs";
   };
 
-  # neovim config
-  # xdg.configFile.nvim = {
-  #   source = ./neovim;
-  #   recursive = true;
-  # };
+  nix.registry = {
+    # redo nixpkgs to be stable
+    nixpkgs = {
+      from = {
+        type = "indirect";
+        id = "nixpkgs";
+      };
+      to = {
+        type = "github";
+        owner = "NixOS";
+        repo = "nixpkgs";
+        ref = "nixos-23.11"; # TODO: make this a variable somewhere
+      };
+    };
+
+    # add nixpkgs unstable
+    unstable = {
+      from = {
+        type = "indirect";
+        id = "unstable";
+      };
+      to = {
+        type = "github";
+        owner = "NixOS";
+        repo = "nixpkgs";
+        ref = "nixos-unstable";
+      };
+    };
+  };
 
   # for discord, nvidia drivers
   nixpkgs.config.allowUnfree = true;
