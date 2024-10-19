c9fae1b655c3727805fe7e83c34f5377e0047858
rutrum <dave@rutrum.net>
2024-10-19 09:33:39 -0400

    Sync notes

--- hosts/rumtower/configuration.nix
+++ hosts/rumtower/configuration.nix
@@ -32,7 +32,6 @@
   # This should be a module
   fonts.fontDir.enable = true;
 
-  # syncthing
   services.syncthing = {
     enable = true;
     user = "rutrum";
@@ -55,7 +54,7 @@
         notes = {
           id = "mqkjy-xoe93";
           path = "/mnt/barracuda/notes";
-          devices = [ "rumbeta" ];
+          devices = [ "rumbeta" "rumprism" ];
         };
         prism-instances = {
           id = "cdgrh-cn25a";
