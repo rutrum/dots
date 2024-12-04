43b895343d38b27b6e996a2d5bd053ac3a394da0
rutrum <dave@rutrum.net>
2024-12-04 12:07:18 -0500

    Fix icons and refactor urls

--- hosts/modules/services/dashy.nix
+++ hosts/modules/services/dashy.nix
@@ -1,4 +1,9 @@
 { pkgs, config, lib, ... }: let
+  rumnas = "http://rumnas.lynx-chromatic.ts.net";
+  rumnas_ip = "http://192.168.50.3";
+  rumtower = "http://rumtower.lynx-chromatic.ts.net";
+  rumpi_ip = "http://192.168.50.2";
+
   dashy_conf = pkgs.writers.writeYAML "conf.yml" {
     appConfig = {
       language = "en";
@@ -19,49 +24,49 @@
             title = "AdGuard Home";
             description = "Local DNS and ad blocker";
             icon = "hl-adguard-home";
-            url = "http://192.168.50.3:3001"; # tailscale address doesn't work for this one?
+            url = "${rumnas_ip}:3001"; # tailscale address doesn't work for this one?
           }
           {
             title = "Jellyfin";
             description = "Home media server";
-            icon = "http://rumtower.lynx-chromatic.ts.net:8096/web/bc8d51405ec040305a87.ico";
-            url = "http://rumnas.lynx-chromatic.ts.net:8096";
+            icon = "hl-jellyfin";
+            url = "${rumtower}:8096";
           }
           {
             title = "TabbyML";
             description = "AI code assistant";
             icon = "https://raw.githubusercontent.com/TabbyML/tabby/main/website/static/img/favicon.ico";
-            url = "http://rumnas.lynx-chromatic.ts.net:11029";
+            url = "${rumnas}:11029";
           }
           {
             title = "Octoprint";
             description = "3D printing";
             icon = "hl-octoprint";
-            url = "http://192.168.50.2:5000";
+            url = "${rumpi_ip}:5000";
           }
           {
             title = "NocoDB";
             description = "No code database";
-            icon = "http://rumnas.lynx-chromatic.ts.net:8081/dashboard/_nuxt/256x256.DJId9Ub8.png";
-            url = "http://rumnas.lynx-chromatic.ts.net:8081";
+            icon = "https://raw.githubusercontent.com/nocodb/nocodb/refs/heads/develop/packages/nc-gui/assets/img/icons/64x64.png";
+            url = "${rumnas}:8081";
           }
           {
             title = "Home Assistant";
             description = "Home automation";
             icon = "hl-home-assistant";
-            url = "http://rumnas.lynx-chromatic.ts.net:8082";
+            url = "${rumnas}:8082";
           }
           {
             title = "Paperless";
             description = "Document organization";
             icon = "hl-paperless";
-            url = "http://rumtower.lynx-chromatic.ts.net:8000";
+            url = "${rumtower}:8000";
           }
           {
             title = "Firefly";
             description = "Personal finance tracker";
             icon = "hl-firefly";
-            url = "http://rumtower.lynx-chromatic.ts.net:8080";
+            url = "${rumtower}:8080";
           }
           {
             title = "Router";
@@ -73,25 +78,25 @@
             title = "Tube Archivist";
             description = "Local YouTube video library";
             icon = "hl-tube-archivist";
-            url = "http://rumnas.lynx-chromatic.ts.net:8090";
+            url = "${rumnas}:8090";
           }
           {
             title = "Grafana";
             description = "Data visualization";
             icon = "hl-grafana";
-            url = "http://rumnas.lynx-chromatic.ts.net:3000";
+            url = "${rumnas}:3000";
           }
           {
             title = "Open WebUI";
             description = "LLM chat interface";
             icon = "hl-open-webui";
-            url = "http://rumnas.lynx-chromatic.ts.net:8080";
+            url = "${rumnas}:8080";
           }
           {
             title = "Mealie";
             description = "Recipe manager";
             icon = "hl-mealie";
-            url = "http://rumnas.lynx-chromatic.ts.net:9000";
+            url = "${rumnas}:9000";
           }
         ];
       }
@@ -103,7 +108,6 @@
             description = "Cloud suite";
             icon = "hl-nextcloud";
             url = "https://cloud.rutrum.net";
-            target = "newtab";
           }
           {
             title = "Paradisus Docs";
@@ -119,13 +123,14 @@
           }
           {
             title = "stringcase.org";
-            icon = "hl-spring_creators";
             url = "https://stringcase.org";
+            icon = "favicon";
           }
           {
             title = "rutrum.net";
             description = "Dave's personal site";
             url = "https://rutrum.net";
+            icon = "favicon";
           }
         ];
       }
@@ -136,40 +141,42 @@
             title = "Vultr";
             description = "Cloud provider";
             url = "https://my.vultr.com";
-            icon = "si-vultr";
+            icon = "hl-vultr";
           }
           {
             title = "Namecheap";
             description = "DNS registrar";
             url = "https://www.namecheap.com/myaccount/login";
+            icon = "favicon";
           }
           {
             title = "Tailscale";
             description = "Mesh VPN service";
             url = "https://login.tailscale.com/login";
-            icon = "si-tailscale";
+            icon = "favicon";
           }
           {
             title = "Proton";
             description = "Email provider";
             url = "https://account.proton.me/login";
-            icon = "si-proton";
+            icon = "favicon";
           }
           {
             title = "BorgBase";
             description = "Encrypted cloud backup";
             url = "https://borgbase.com/login";
-            icon = "si-borgbase";
+            icon = "favicon";
           }
           {
             title = "SimpleLogin";
             description = "Email aliasing";
             url = "https://app.simplelogin.io/auth/login";
-            icon = "si-simplelogin";
+            icon = "favicon";
           }
           {
             title = "Mullvad";
             description = "VPN service";
+            icon = "favicon";
             url = "https://mullvad.net/en/account/login";
           }
         ];
@@ -189,7 +196,6 @@ in {
         ports = [ "${toString config.dashy.port}:8080" ];
         autoStart = true;
         volumes = [
-          #"/root/dashy_config:/app/user-data"
           "${dashy_conf}:/app/user-data/conf.yml"
         ];
         environment = {
