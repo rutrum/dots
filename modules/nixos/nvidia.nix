4b2003237e9386d183eeecbbe4b93552b138735b
rutrum <dave@rutrum.net>
2025-11-28 13:02:54 -0500

    Fix nvidia

--- modules/nixos/nvidia.nix
+++ modules/nixos/nvidia.nix
@@ -13,12 +13,11 @@
   hardware.nvidia-container-toolkit.enable = true;
 
   hardware.nvidia = {
+    open = true;
     modesetting.enable = true;
     powerManagement.enable = false;
     powerManagement.finegrained = false;
-    open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.production;
   };
 
   nixpkgs.config.allowUnfree = true;
