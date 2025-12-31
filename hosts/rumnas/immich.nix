ce0d85e1b9cfc09eaed53027c6ba59c0025aef07
rutrum <dave@rutrum.net>
2025-12-31 05:48:22 -0500

    Add immich-kiosk

--- hosts/rumnas/immich.nix
+++ hosts/rumnas/immich.nix
@@ -9,4 +9,16 @@
   };
 
   users.users.immich.extraGroups = ["video" "render"];
+
+  virtualisation.oci-containers.containers = {
+    immich-kiosk = {
+      image = "ghcr.io/damongolding/immich-kiosk:latest";
+      ports = ["9019:3000"];
+      environment = {
+        KIOSK_IMMICH_URL = "http://host.containers.internal:2283";
+        KIOSK_IMMICH_API_KEY = "F1rt9bxYZu3WIcIHMatbpkNWU9fsmPHwUhDfEY4HRNQ";
+      };
+      autoStart = true;
+    };
+  };
 }
