bca2ba6597afa82e561a4556a05d5753c852c286
rutrum <dave@rutrum.net>
2026-01-25 20:40:58 -0500

    Paperless container -> nixpkgs module

--- hosts/rumtower/paperless.nix
+++ hosts/rumtower/paperless.nix
@@ -1,39 +1,17 @@
-{
-  config,
-  pkgs,
-  inputs,
-  ...
-}: {
-  systemd.services = inputs.self.lib.mkPodmanNetwork config "paperless";
+{perSystem, ...}: {
+  services.paperless = {
+    enable = true;
+    package = perSystem.nixpkgs-unstable.paperless-ngx;
+    address = "0.0.0.0";
+    port = 8000;
 
-  # the containers
-  virtualisation.oci-containers.containers = {
-    paperless = {
-      image = "ghcr.io/paperless-ngx/paperless-ngx:2.20.0";
-      ports = ["8000:8000"];
-      volumes = [
-        "/mnt/barracuda/paperless/data:/usr/src/paperless/data"
-        "/mnt/barracuda/paperless/media:/usr/src/paperless/media"
-        "/mnt/barracuda/paperless/export:/usr/src/paperless/export"
-        "/mnt/barracuda/paperless/consume:/usr/src/paperless/consume"
-      ];
-      environment = {
-        PAPERLESS_REDIS = "redis://paperless-redis:6379";
-        # removed digital signatures for OCR
-        # see the original version of the document for unmodified version
-        PAPERLESS_OCR_USER_ARGS = ''{"invalidate_digital_signatures": true}'';
-      };
-      dependsOn = ["paperless-redis"];
-      autoStart = true;
-      #user = "1000";
-      networks = ["paperless"];
-    };
-    paperless-redis = {
-      image = "library/redis:latest"; # official docker images use "library/"
-      volumes = ["redisdata:/data"];
-      autoStart = true;
-      #user = "1000";
-      networks = ["paperless"];
+    dataDir = "/mnt/barracuda/paperless/data";
+    mediaDir = "/mnt/barracuda/paperless/media";
+    consumptionDir = "/mnt/barracuda/paperless/consume";
+
+    settings = {
+      # Preserve OCR setting for digital signatures
+      PAPERLESS_OCR_USER_ARGS = ''{"invalidate_digital_signatures": true}'';
     };
   };
 }
