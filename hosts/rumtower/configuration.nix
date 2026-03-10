12e0ff2d58e0b74089ede59e15f338b9f92cb1ac
rutrum <dave@rutrum.net>
2026-03-10 05:04:12 -0400

    Fix for kde login error

--- hosts/rumtower/configuration.nix
+++ hosts/rumtower/configuration.nix
@@ -31,6 +31,9 @@
   networking.hostName = "rumtower";
   networking.hosts."192.168.50.3" = ["rumnas" "rum.internal"];
 
+  # Disable Baloo file indexing - causes repeated crashes
+  environment.plasma6.enableBalooFileIndex = false;
+
   services = {
     xserver.enable = true;
     # maybe revisit when cursor bug is fixed
